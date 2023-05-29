2789ad90c0a2fa32c680f4560fc603e82ac4757e
Ritam Ghosh <ritamghosh97@gmail.com>
2023-05-29 19:44:22 +0530

    Timesheet Rest API: Initial setup.

---
+++ src/main/java/com/exlservice/timesheet/ExlTspRestServiceApplication.java
@@ -0,0 +1,11 @@
+package com.exlservice.timesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExlTspRestServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExlTspRestServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/exlservice/timesheet/ExlTspRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exlservice.timesheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExlTspRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
