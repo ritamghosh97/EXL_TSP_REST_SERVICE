68cd15ef6171508a7e22baacf5fc3b21bfc4fdeb
Ritam Ghosh <ritamghosh97@gmail.com>
2023-05-29 19:47:03 +0530

    Timesheet Rest API: Created Employee and Timesheet entity class and mapped to employee table and timesheet table respectively. Add established one-to-many relationship between them.

---
+++ src/main/java/com/exlservice/timesheet/entity/Employee.java
@@ -0,0 +1,97 @@
+package com.exlservice.timesheet.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="manager_id")
+    private Integer managerId;
+
+    @OneToMany(mappedBy="employee", cascade= {CascadeType.DETACH,
+            CascadeType.MERGE, CascadeType.REMOVE
+    })
+    private List<Timesheet> timesheet;
+
+    public Employee() {
+
+    }
+
+    public Employee(int id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int empId) {
+        this.id = empId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(Integer managerId) {
+        this.managerId = managerId;
+    }
+
+    public List<Timesheet> getTimesheet() {
+        return timesheet;
+    }
+
+    public void setTimesheet(List<Timesheet> timesheet) {
+        this.timesheet = timesheet;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [empId=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+                + ", managerId=" + managerId + "]";
+    }
+}
---
+++ src/main/java/com/exlservice/timesheet/entity/Timesheet.java
@@ -0,0 +1,96 @@
+package com.exlservice.timesheet.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="timesheet")
+public class Timesheet {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    private Integer timesheetId;
+
+    @Column(name = "date")
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private LocalDate date;
+
+    @Column(name = "hours")
+    private Integer hours;
+
+    @Column(name = "approval_status")
+    private int approvalStatus;
+
+    @ManyToOne(cascade= {CascadeType.DETACH,
+            CascadeType.REFRESH})
+    @JoinColumn(name = "emp_id")
+    @JsonIgnore
+    private Employee employee;
+
+    public Timesheet() {
+
+    }
+
+    public Timesheet(Integer id, LocalDate date, Integer hours, Employee employee) {
+        this.timesheetId = id;
+        this.date = date;
+        this.hours = hours;
+        this.employee = employee;
+    }
+
+    public Integer getTimesheetId() {
+        return timesheetId;
+    }
+
+    public void setTimesheetId(Integer timesheetId) {
+        this.timesheetId = timesheetId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
+
+    public int getApprovalStatus() {
+        return approvalStatus;
+    }
+
+    public void setApprovalStatus(int approvalStatus) {
+        this.approvalStatus = approvalStatus;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public String toString() {
+        return "Timesheet{" +
+                "timesheetId=" + timesheetId +
+                ", date=" + date +
+                ", hours=" + hours +
+                ", approvalStatus=" + approvalStatus +
+                ", employee=" + employee +
+                '}';
+    }
+}
