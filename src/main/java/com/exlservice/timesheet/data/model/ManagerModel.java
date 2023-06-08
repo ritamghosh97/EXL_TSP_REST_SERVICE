9aa156fe3b402eca5023487925f294f9dd255572
Ritam Ghosh <ritamghosh97@gmail.com>
2023-06-08 19:08:21 +0530

    Created a model class called EmployeeModelForManager to format the JSON Response for manager.

---
+++ src/main/java/com/exlservice/timesheet/data/model/ManagerModel.java
@@ -0,0 +1,119 @@
+package com.exlservice.timesheet.data.model;
+
+import com.exlservice.timesheet.entity.Employee;
+
+import java.util.List;
+import java.util.Set;
+
+public class ManagerModel {
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private int managerId;
+
+    private Set<String> roles;
+
+    private List<String> weekDates;
+
+    private List<EmployeeModelForManager> employees;
+
+    public ManagerModel(){
+
+    }
+
+    public ManagerModel(Integer id, String firstName, String lastName,
+                        String email, int managerId, Set<String> roles,
+                        List<String> weekDates, List<EmployeeModelForManager> employees) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.managerId = managerId;
+        this.roles = roles;
+        this.weekDates = weekDates;
+        this.employees = employees;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(int managerId) {
+        this.managerId = managerId;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+
+    public List<String> getWeekDates() {
+        return weekDates;
+    }
+
+    public void setWeekDates(List<String> weekDates) {
+        this.weekDates = weekDates;
+    }
+
+    public List<EmployeeModelForManager> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<EmployeeModelForManager> employees) {
+        this.employees = employees;
+    }
+
+    @Override
+    public String toString() {
+        return "ManagerModel{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", managerId='" + managerId + '\'' +
+                ", roles=" + roles +
+                ", weekDates=" + weekDates +
+                ", employees=" + employees +
+                '}';
+    }
+}
